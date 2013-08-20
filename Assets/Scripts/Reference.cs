c05289b7deecc82f0a558da3d8715b90f73209ac
Matthew Woo <MatthewC.Woo@gmail.com>
2013-08-20 12:37:16 -0700

    Changed to recycle projectiles

--- Assets/Scripts/Reference.cs
+++ Assets/Scripts/Reference.cs
@@ -1,3 +1,4 @@
+//mwoo_dev
 using UnityEngine;
 using System.Collections.Generic;
 
@@ -7,11 +8,13 @@ public struct ElementAttributes{
   public HashSet<Element> weakness; //NOTE do we even need this?
   public HashSet<Element> strength;
   public HashSet<Element> creates;
+  public Material mat;
 
-  public ElementAttributes(HashSet<Element> weakness, HashSet<Element> strength, HashSet<Element> creates){
+  public ElementAttributes(HashSet<Element> weakness, HashSet<Element> strength, HashSet<Element> creates, Material mat){
     this.weakness = weakness;
     this.strength = strength;
     this.creates = creates;
+    this.mat = mat;
   }
 }
 
@@ -36,15 +39,23 @@ static class Reference {
   public static HashSet<Element> CreateEarth = new HashSet<Element> { Element.metal };
   public static HashSet<Element> CreateMetal = new HashSet<Element> { Element.water };
   public static HashSet<Element> CreateHoly  = new HashSet<Element> { };
-	
+
+  public static Material waterMat = Resources.Load("Materials/waterMat") as Material;
+  public static Material fireMat = Resources.Load("Materials/fireMat") as Material;
+  public static Material woodMat = Resources.Load("Materials/woodMat") as Material;
+  public static Material earthMat = Resources.Load("Materials/earthMat") as Material;
+  public static Material metalMat = Resources.Load("Materials/metalMat") as Material;
+  public static Material holyMat = Resources.Load("Materials/holyMat") as Material;
+  public static Material normalMat = Resources.Load("Materials/normalMat") as Material;
+
   public static Dictionary<Element, ElementAttributes> elements =
     new Dictionary<Element, ElementAttributes>{
-    { Element.water , new ElementAttributes(WeaknessWater, StrengthWater, CreateWater) },
-    { Element.fire  , new ElementAttributes(WeaknessFire,  StrengthFire,  CreateFire) },
-    { Element.wood  , new ElementAttributes(WeaknessWood,  StrengthWood,  CreateWood) },
-    { Element.earth , new ElementAttributes(WeaknessEarth, StrengthEarth, CreateEarth) },
-    { Element.metal , new ElementAttributes(WeaknessMetal, StrengthMetal, CreateMetal) },
-    { Element.holy  , new ElementAttributes(WeaknessHoly,  StrengthHoly,  CreateHoly) }
+    { Element.water , new ElementAttributes(WeaknessWater, StrengthWater, CreateWater, waterMat) },
+    { Element.fire  , new ElementAttributes(WeaknessFire,  StrengthFire,  CreateFire, fireMat) },
+    { Element.wood  , new ElementAttributes(WeaknessWood,  StrengthWood,  CreateWood, woodMat) },
+    { Element.earth , new ElementAttributes(WeaknessEarth, StrengthEarth, CreateEarth, earthMat) },
+    { Element.metal , new ElementAttributes(WeaknessMetal, StrengthMetal, CreateMetal, metalMat) },
+    { Element.holy  , new ElementAttributes(WeaknessHoly,  StrengthHoly,  CreateHoly, holyMat) }
   };
 
   //TODO the other elements
